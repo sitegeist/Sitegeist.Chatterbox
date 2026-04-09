761d27645433c0848528808cea408891e9111c25
Martin Ficzel <ficzel@sitegeist.de>
2026-04-09 17:06:30 +0200

    TASK: Seperate methods for effort as string or as enum

--- Classes/Domain/Assistant.php
+++ Classes/Domain/Assistant.php
@@ -88,7 +88,7 @@ final class Assistant
         ];
 
         if ($reasoningEffort = $this->entity->getReasoningEffort()) {
-            $responseParameters['reasoning']['effort'] = $reasoningEffort->value;
+            $responseParameters['reasoning']['effort'] = $reasoningEffort;
         }
 
         $this->logger?->info("thread create response", $responseParameters);
